31becb6d7ca23e5e06f4fccaf85d51fb6b483452
shafiqshuvo <shafiq.shuvo.nstu.ice@gmail.com>
2019-10-16 12:55:45 +0600

    Salary Calculator

---
+++ SalaryCalculatorAppPractice3/Salary.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryCalculatorAppPractice3
+{
+    class Salary
+    {
+        //public string employeeName;
+        //public double basicAmount;
+        //public double houseRentPercent;
+        //public double medicalAllowancePercent;
+        public string Name { set; get; }
+        public double Basic { set; get; }
+        public double Houserent { set; get; }
+        public double MedicalAllowance { set; get; }
+
+        public string GetSalary()
+        {
+            double salary = Basic + (Basic * Houserent / 100) +
+                            (Basic * MedicalAllowance / 100);
+
+            string message = Name + " your salary is: " + salary;
+
+            return message;
+        }
+    }
+}
---
+++ SalaryCalculatorAppPractice3/SalaryCalculatorUi.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SalaryCalculatorAppPractice3
+{
+    public partial class SalaryCalculatorUi : Form
+    {
+        Salary salary = new Salary();
+        public SalaryCalculatorUi()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowMeSalaryButton_Click(object sender, EventArgs e)
+        {
+
+            salary.Name = employeeNameTextBox.Text;
+            salary.Basic = Convert.ToDouble(basicAmountTextBox.Text);
+            salary.Houserent = Convert.ToDouble(homeRentTextBox.Text);
+            salary.MedicalAllowance = Convert.ToDouble(medicalAllowanceTextBox.Text);
+
+            MessageBox.Show(salary.GetSalary());
+        }
+    }
+}
